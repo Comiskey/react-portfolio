be334ca90ab7a38f02c62d13a0c9c075fe2743f0
Evan Comiskey <comiskey@mit.edu>
2024-08-06 10:35:46 -0400

    Altered vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/<repository-name>/' : '/',
+  base: '/react-portfolio/',
   plugins: [react()],
   css:{
     modules: {
